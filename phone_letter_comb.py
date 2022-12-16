027190922bb1255921a607a025e605d0362985ef
jaanisfehling <57713516+jaanisfehling@users.noreply.github.com>
2022-12-16 15:59:19 +0100

    Create phone_letter_comb.py

---
+++ phone_letter_comb.py
@@ -0,0 +1,22 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0: return []
+        mapper = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
+        }
+
+        res = [mapper[x] for x in digits]
+        out = [""]
+
+        for sub in res:
+            out = [x+y for x in out for y in sub]
+        return out
+
+
